b8cd9b05d082b0ddbf0e3aa994aaa51bf808a8e7
SalCilia <scilia@gmail.com>
2020-05-04 14:26:40 -0700

    published Dog API to github

---
+++ build/precache-manifest.6a7c19702e51d6f8a58953e6e84e1a0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c835d947047d46ceb8be8f90f89a82c",
+    "url": "/For-Scott/index.html"
+  },
+  {
+    "revision": "a6758448bf8edb4c38fc",
+    "url": "/For-Scott/static/css/main.87107a4e.chunk.css"
+  },
+  {
+    "revision": "409cd0a24cbbe9097b82",
+    "url": "/For-Scott/static/js/2.8fc2e5ea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/For-Scott/static/js/2.8fc2e5ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6758448bf8edb4c38fc",
+    "url": "/For-Scott/static/js/main.3cde1043.chunk.js"
+  },
+  {
+    "revision": "8a27e6809859a662e651",
+    "url": "/For-Scott/static/js/runtime-main.a0b44d20.js"
+  }
+]);
